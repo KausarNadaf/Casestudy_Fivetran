8833ae58fbaf83ed9e5516db8b6e4274f7e37d6b
Kausar Nadaf <Kausar.Nadaf@ibm.com>
2025-04-17 10:45:46 +0000

    -

--- models/Casestudy1/case1.sql
+++ models/Casestudy1/case1.sql
@@ -1,15 +1,15 @@
 SELECT
     t1.Product_ID AS Product_ID_t1,
-    t2.Product_ID AS Product_ID_t2,
     t1.Product_Name AS Product_Name_t1,
-    t2.Product_Name AS Product_Name_t2,
     t1.Category AS Category_t1,
-    t2.Category AS Category_t2,
     t1.Price AS Price_t1,
-    t2.Price AS Price_t2,
     t1.Stock AS Stock_t1,
-    t2.Stock AS Stock_t2
 FROM MYDB.ONE_DRIVE.PRODUCT_DATA_SHEET_1 AS t1
-FULL OUTER JOIN MYDB.CASESTUDY_CASESTUDY.STG_PRODUCTS AS t2
-    ON t1.Product_ID = t2.Product_ID 
-    
+UNION SELECT  
+    t2.Product_ID AS Product_ID_t2,
+    t2.Product_Name AS Product_Name_t2,
+    t2.Category AS Category_t2,
+    t2.Price AS Price_t2,
+     t2.Stock AS Stock_t2
+  FROM  MYDB.CASESTUDY_CASESTUDY.STG_PRODUCTS AS t2
+
